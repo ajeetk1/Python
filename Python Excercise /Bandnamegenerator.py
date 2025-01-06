0c38e839af60cfaf21226e338ff01ce6029d7380
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2025-01-06 23:16:27 +0530

    Python Excercise

---
+++ Python Excercise /Bandnamegenerator.py
@@ -0,0 +1,6 @@
+print ("Brand Name Generator")
+name = input("What is your brand name")
+pet  = input("What is your pet name")
+city = input("Which city you are in")
+
+print(f"My{name} is {pet} and {city}")
