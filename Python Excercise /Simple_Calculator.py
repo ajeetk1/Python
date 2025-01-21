54c57636d7e386449d8860ff649a5adca194d0fe
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2025-01-21 20:02:36 +0530

    Python - Calculator

---
+++ Python Excercise /Simple_Calculator.py
@@ -0,0 +1,12 @@
+# Create a simple calculator in Python
+# Define the calculator 
+def Calculator():
+   print("Welcome to the Calculator:")
+   print("Start the operation:")
+   # Addition Operation 
+   print("1: Press to do the Addition (+)")
+   print("2: Press to do the Substraction (-)")
+   print("3: Press to do the Multiply (*)")
+   print("4: Press to do the Division (/)")
+
+   # Start the operation 
