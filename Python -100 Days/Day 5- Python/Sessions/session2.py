42e29a8868dd8a0c946e2d74e5d6528a5507adb1
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2025-09-15 18:31:59 +0530

    100 Days of Python- Day 6

--- Python -100 Days/Day 5- Python/Sessions/session2.py
+++ Python -100 Days/Day 5- Python/Sessions/session2.py
@@ -10,3 +10,19 @@ for number in range (1,12):
   # range(start,stop,step)
   # Meaning: start at 1, then keep adding 11, until it reaches or passes 10.First value: 1 Next would be 1 + 11 = 12 → but 12 ≥ 10, so the loop ends.
   #So the output should be just 1, not 1,1,1,2.
+
+  for number in range(1,1,2):
+    print(number)
+
+  for number in range (1,11,3):
+# start 1 ,stop 11,step 3
+# start 1 + step = 4 + step = 7 +3 =10  and 10+3 = 13>stop 11 
+    print(number)
+
+
+  ## 
+
+  total = 0
+  for number in range (1,101): # 1 to 100
+    total+= number # total = total+number 0 = 0+1 , 1 +2 =3; 
+    print(total)
