ca26b5a5cfc67b419185673d375b628da4bc98de
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2025-09-21 15:41:45 +0530

    100 Days of Python

--- Python -100 Days/Day 7- Python /Challenges/cha02.py
+++ Python -100 Days/Day 7- Python /Challenges/cha02.py
@@ -1,3 +1,23 @@
+# Challenge 2
+
+import random 
+
+word_list = ["adda",'jabba',"pucca"]
+
+chosen_word = random.choice(word_list)
+
+display = ["-"]*len(chosen_word)
+print(display)
+
+guess_letter = input("Enter the letter").lower()
+
+for idx,letter in enumerate(chosen_word):
+    if guess_letter == letter:
+        display[idx] = letter
+
+print(display)
+
+
 
 
 
