fb036838238addbc70dc41723787c8ccfb415e99
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2025-09-23 15:37:33 +0530

    100 Days of Code - Day8

--- Python -100 Days/Day 8- Python/Exercises/exce01.py
+++ Python -100 Days/Day 8- Python/Exercises/exce01.py
@@ -1,5 +1,6 @@
 # Mathemics Function  - Day8
 # We are creating math and ceil 
+# ceil round fucntion to nearest whole number
 
 from math import ceil 
 
@@ -18,4 +19,24 @@ print(paint_area_total)
 print(f"Total area is {paint_area_total}")
  
 # # 
+
+from math import ceil 
+
+# Function Declare 
+def rect_calc(length,width):
+   rect_area = ceil(length*width)
+   return rect_area
+
+# user input 
+l = 10
+w= 20 
+
+# Calculation
+
+total_area = rect_calc(l,w)
+print(total_area)
+
+
+
+
 
