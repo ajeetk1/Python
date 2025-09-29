7c668df4be9cf50e78cd4d41a5e685262a197c55
Ajeet Khamesra <ajeet.khamesra1@outlook.com>
2025-09-29 23:15:22 +0530

    100 Days of code - Python

--- Python -100 Days/Day 8- Python/Projects/ToDoList.py
+++ Python -100 Days/Day 8- Python/Projects/ToDoList.py
@@ -4,3 +4,21 @@
 
 
 
+# Super Basic Project Random Number
+
+import random 
+
+number = random.randint(1,10)
+guess = int(input("Enter your guessed number?"))
+
+if number == guess:
+    print("You won")
+else:
+    print("You lost")
+
+
+
+
+
+
+
